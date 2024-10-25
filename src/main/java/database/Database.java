dcc61a1fc2d921596ff8ea5e167140f993c56cca
anna schwarz <anousheh.y23@gmail.com>
2024-10-25 14:39:01 +0330

    Add TweetList to DataBase

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -1,10 +1,12 @@
 package database;
 
+import Entity.TweetList;
 import Entity.User;
 import Entity.UserList;
 
 public class Database {
     public final static UserList  userList = new UserList();
+    public final static TweetList tweetList = new TweetList();
 
     public static User loggedInUser;
 }
