b93464f1ee6b39198d17e64f1fd9bb3239019281
anna schwarz <anousheh.y23@gmail.com>
2024-10-25 15:31:59 +0330

    Add likedTweet method

---
+++ src/main/java/service/TweetService.java
@@ -0,0 +1,24 @@
+package service;
+
+import Entity.Tweet;
+import Entity.User;
+
+public class TweetService {
+    public void showTweet(Tweet tweet) {
+        int likes = tweet.getLikedUsers().getIndex();
+        String authorName = tweet.getUser().getFirstName() +" "+ tweet.getUser().getLastName();
+        System.out.println(tweet.getText()+"\n"+authorName+"\n"+likes );
+
+    }
+
+    public boolean isUserLiked(Tweet tweet, User user) {
+        for(int i=0; i<tweet.getLikedUsers().getIndex(); i++) {
+            User likedUser = tweet.getLikedUsers().getUsers(i);
+            if(likedUser.equals(user)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
