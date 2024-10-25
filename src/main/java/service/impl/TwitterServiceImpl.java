55eeec975f7744056703adf3dfd519937cbe164f
anna schwarz <anousheh.y23@gmail.com>
2024-10-25 14:59:44 +0330

    Improve displayAllTweet method

--- src/main/java/service/impl/TwitterServiceImpl.java
+++ src/main/java/service/impl/TwitterServiceImpl.java
@@ -1,14 +1,18 @@
 package service.impl;
 
-import Entity.User;
+import service.TweetService;
 import service.TwitterService;
+import static database.Database.tweetList;
 
 public class TwitterServiceImpl implements TwitterService {
-
+    TweetService tweetService = new TweetService();
     @Override
     public void displayAllTweet() {
-
         //show likes
         //show author of tweet
+        for(int i=0; i< tweetList.getIndex();i++){
+            tweetService.showTweet(tweetList.getTweets(i));
+            System.out.println("-------");
+        }
     }
 }
