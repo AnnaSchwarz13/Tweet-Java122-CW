af9f5517649c1460cd177e5a9574f62a2396a468
anna schwarz <anousheh.y23@gmail.com>
2024-10-25 14:19:44 +0330

    Tweet class

---
+++ src/main/java/service/impl/TwitterServiceImpl.java
@@ -0,0 +1,21 @@
+package service.impl;
+
+import Entity.User;
+import service.TwitterService;
+
+public class TwitterServiceImpl implements TwitterService {
+    @Override
+    public void Login(User user) {
+        
+    }
+
+    @Override
+    public void tweet(String text) {
+
+    }
+
+    @Override
+    public void displayAllTweet() {
+
+    }
+}
