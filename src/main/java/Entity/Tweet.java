4a29807df6bb61d9a3c9587dde2135726435cc57
anna schwarz <anousheh.y23@gmail.com>
2024-10-25 14:39:50 +0330

    Creat a Constructor in TweetClass

--- src/main/java/Entity/Tweet.java
+++ src/main/java/Entity/Tweet.java
@@ -1,17 +1,21 @@
 package Entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @ToString
 public class Tweet {
 
+    private UserList likedUsers;
     private String text;
     private User user;
 
+    public Tweet(String text, User user) {
+        this.text = text;
+        this.user = user;
+        this.likedUsers = new UserList();
+    }
 }
